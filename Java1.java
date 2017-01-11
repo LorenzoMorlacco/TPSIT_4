855d8ff4f92d7a631bb19aa0aefe0acb9ea4c2df
LorenzoMorlacco <lorenzomorlacco@gmail.com>
2017-01-11 08:17:30 +0100

    Create Java1.java

---
+++ Java1.java
@@ -0,0 +1,15 @@
+package java1;
+/**
+ *
+ * @author Lorenzo Morlacco
+ */
+public class Java1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Hello Word!");
+    }
+    
+}
